b3453d977732975235f134ee4518e22f9cd485ac
ringeringeraja <joaosan177@gmail.com>
2023-01-10 00:08:15 -0300

    npm

---
+++ v1/index.js
@@ -0,0 +1,4 @@
+export { default as AccessControlSchema } from './specs/access-control.schema.json'
+export { default as ApiConfigSchema } from './specs/api-config.schema.json'
+export { default as DescriptionSchema } from './specs/description.schema.json'
+export { default as FilesystemSchema } from './specs/filesystem.schema.json'
